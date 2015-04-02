1d74c69a527813558599e31cebdde991d6b54f2a
Azmul Hossain <azmulg@gmail.com>
2015-04-01 23:17:58 -0700

    sprite image

---
+++ public/grunt.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		min : {
+
+			dist : {
+                 
+                 src = 'app/controller.js',
+                 dest = 'controller.mim.js'
+			}
+		}
+	});
+
+	
+}
